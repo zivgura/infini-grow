ffc30490c284ad3837b21e7e39caab347c30f429
Ziv <zivgura6@gmail.com>
2024-02-10 16:50:38 +0200

    added support for years in second tab

--- src/components/BudgetRowsPerYear/BudgetRowsPerYear.jsx
+++ src/components/BudgetRowsPerYear/BudgetRowsPerYear.jsx
@@ -6,15 +6,17 @@ import { BudgetRows } from '../BudgetRows/BudgetRows';
 export function BudgetRowsPerYear({rowsData}) {
     const uniqueYears = [...new Set(rowsData?.map(({date}) => moment(date)?.year()))]
     return (
-        uniqueYears?.map((year, index) => (
-            <BudgetRows
+        uniqueYears?.map((year, index) => {
+            const rowsDataByYear = rowsData.filter(({date})=> moment(date)?.year() === year)
+            return (<BudgetRows
                 key={index}
-                rowsData={rowsData}
+                rowsData={rowsDataByYear}
                 yearOfCreation={year}
-            />
-        ))
+            />)
+        })
     )
 }
+
 BudgetRowsPerYear.propTypes = {
     rowsData: PropTypes.arrayOf(RowDataProps).isRequired
 }
